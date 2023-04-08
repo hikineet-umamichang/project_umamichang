2294792854ce7977379f20e64890366cf6ad4700
hikineet-umamichang <1308yuta@gmail.com>
2023-04-08 22:22:09 +0900

    add mail function

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,23 @@
+import os
 from flask import Flask, Blueprint, send_from_directory
 from .main import main as main_blueprint
 from .weather import weather as weather_blueprint
 from .wordwolf import wordwolf as wordwolf_blueprint
 from app.blog import blog as blog_blueprint
+from .mail import mail as mail_blueprint
 
 
 def create_app():
     app = Flask(__name__)
 
+    app.secret_key = os.environ.get("SECRET_KEY")
+
+
     app.register_blueprint(main_blueprint)
     app.register_blueprint(weather_blueprint)
     app.register_blueprint(wordwolf_blueprint, url_prefix="/wordwolf")
     app.register_blueprint(blog_blueprint, url_prefix="/blog")
+    app.register_blueprint(mail_blueprint, url_prefix="/mail")
 
     # blog_posts/images用のBlueprintを作成
     blog_images_bp = Blueprint("blog_images", __name__, url_prefix="/images")
