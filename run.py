4eab97422da9707118e28fcfee30939db3036319
hikineet-umamichang <1308yuta@gmail.com>
2023-04-16 20:47:41 +0900

    delete config

--- run.py
+++ run.py
@@ -7,6 +7,4 @@ dotenv.load_dotenv(".env", override=True)
 app = create_app()
 
 if __name__ == "__main__":
-    host = app.config["HOST"]
-    port = app.config["PORT"]
-    app.run(host=host, port=port)
+    app.run()
