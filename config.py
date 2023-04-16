a6bed883dd11076aaa07f85ac61dbcb08938c5b3
hikineet-umamichang <1308yuta@gmail.com>
2023-04-16 20:20:33 +0900

    change production settings2

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ class BaseConfig:
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    HOST = '0.0.0.0'
+    HOST = "0.0.0.0"
     PORT = 8008
     # 開発環境用の設定
 
 
 class ProductionConfig(BaseConfig):
-
-    # 本番環境用の設定
+    DEBUG = False
+    TESTING = False
